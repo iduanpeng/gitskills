9ec0f5934fc2aa565558d39eee466f04e288def9
iduanpeng <iduanpeng@163.com>
2019-08-13 18:16:37 +0800

    增加：springboot配置

---
+++ src/main/java/com/iduanpeng/SpringBootServerAplication.java
@@ -0,0 +1,11 @@
+package com.iduanpeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServerAplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootServerAplication.class,args);
+    }
+}
