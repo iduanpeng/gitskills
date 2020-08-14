87f037f798e6dfa370ecea572850d8b2d01c7b71
duanpeng <iduanpeng@163.com>
2020-08-14 15:40:37 +0800

    单例验证

--- design/src/main/java/com/iduanpeng/design/singleton/LazySingleton.java
+++ design/src/main/java/com/iduanpeng/design/singleton/LazySingleton.java
@@ -1,5 +1,7 @@
 package com.iduanpeng.design.singleton;
 
+import java.util.Date;
+
 /**
  *
  * instance=new singleton();这条语句并不是一个原子操作
@@ -8,9 +10,11 @@ package com.iduanpeng.design.singleton;
  * 3.将分配好对象的地址指向instance变量，即instance变量的写；// instance = var;
  * 设想一下，如果不使用volatile关键字限制指令的重排序，1-3-2操作，
  * 获得到单例的线程可能拿到了一个空对象，后续操作会有影响！因此需要引入volatile对变量进行修饰。
+ *
+ * volatile 没有实验出来
  */
 public class LazySingleton {
-    private static volatile LazySingleton instance = null;
+    private static /*volatile*/ LazySingleton instance = null;
 
     private LazySingleton(){}
 
@@ -24,4 +28,18 @@ public class LazySingleton {
         }
         return instance;
     }
+
+    public static void main(String[] args) {
+        for (int i = 0;i < 1000 ;i++){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    LazySingleton instance = LazySingleton.getInstance();
+                    System.out.println(instance);
+                }
+            }).start();
+        }
+    }
+
+
 }
